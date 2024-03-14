ee9b5fbd97c3627e1fe598457e8e1c2167d657bb
tshlaine <atasha_pidlaoan@dlsu.edu.ph>
2024-03-14 21:16:34 +0800

    user button dynamic (fixed)

--- src/controller/profile.controller.js
+++ src/controller/profile.controller.js
@@ -1,14 +1,19 @@
 const { get } = require("lodash");
 
 const profilePage = (req, res) => {
-  const {
-    fullname: { lastname, firstname },
-    username,
-    profilepicture,
-    bio,
-  } = get(req, "identity");
+  // Extract necessary data from req.identity
+  const { fullname: { lastname, firstname }, username, profilepicture, bio } = get(req, "identity");
 
+  // Check the value of profilepicture
+  console.log("Profile Picture:", profilepicture);
+
+  // Set pic to profilepicture if available, otherwise set it to a default value
   const pic = profilepicture ?? "/images/user.png";
+
+  // Log the value of pic
+  console.log("Pic:", pic);
+
+  // Render the profile page with necessary data
   res.render("profile", { lastname, firstname, username, pic, bio });
 };
 
